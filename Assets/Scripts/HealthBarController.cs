74c4c4502169cccf30858b680eefc49244c682ab
fpawlowski <70605021+fpawlowski@users.noreply.github.com>
2022-04-11 14:03:40 -0500

    Created sample scene with health bar that decrements on collision
    Still need to code game ending when bar reaches 0

--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class HealthBarController : MonoBehaviour
 {
     private static Image HealthBarImage;
+    private static int start = 0;
 
     public static void SetHealthValue(float value){
         HealthBarImage.fillAmount = value;
 
-        if(HealthBarImage.fillAmount < 0.2f){
+        if (HealthBarImage.fillAmount <= 0.0f)
+        {
+            //end game
+        }
+        else if(HealthBarImage.fillAmount < 0.2f){
             SetBarColor(Color.red);
         }else if(HealthBarImage.fillAmount < 0.4f){
             SetBarColor(Color.yellow);
@@ -18,9 +23,29 @@ public class HealthBarController : MonoBehaviour
             SetBarColor(Color.green);
         }
     }
+    public static void HitDetected()
+    {
+        float v;
+        if (start < 2)
+        {
+            SetHealthValue(1.0f);
+            start++;
+        }
+        else
+        {
+            v = GetValue();
+            v -= 0.1f;
+            SetHealthValue(v);
+        }
+    }
 
     public static void SetBarColor(Color healthColor){
         HealthBarImage.color = healthColor;
+    }
+
+    public static float GetValue()
+    {
+        return HealthBarImage.fillAmount;
     }
 
     // Start is called before the first frame update
--- Assets/Scripts/ghostCollider.cs
+++ Assets/Scripts/ghostCollider.cs
@@ -7,5 +7,6 @@ public class ghostCollider : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("hit detected");
+        HealthBarController.HitDetected();
     }
 }
